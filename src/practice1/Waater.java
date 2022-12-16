37eec527b5111af422e19ca7cc87f7a968fdb72b
Yunus Demir <yunusdemir2009@gmail.com>
2022-12-16 05:27:29 -0500

    water update

--- src/practice1/Waater.java
+++ src/practice1/Waater.java
@@ -2,6 +2,6 @@ package practice1;
 
 public class Waater {
     // water is life.
-    // we have to keep water clear.
-    //
+    // we have to keep water clean.
+    // we have to make most of people notice the re-cycling.
 }
